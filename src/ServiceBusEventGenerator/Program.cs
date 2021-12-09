5eda71e64d5af0910e895b47debc833c403c08dc
andrewschwabe <schwabea530@gmail.com>
2021-12-09 03:34:59 -0600

    Start of ServiceBus event generator

---
+++ src/ServiceBusEventGenerator/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.ServiceBus;
+using ServiceBusModel;
+using System.Text.Json;
+
+var connectionString = "<ServiceBus connection string>";
+var queueName = "greeter";
+
+var queueClient = new QueueClient(connectionString, queueName);
+
+var person = new Person {
+
+    FirstName = "Andrew",
+    LastName =  "Test"
+};
+
+var message = new Message {
+    Body =  JsonSerializer.SerializeToUtf8Bytes(person)
+};
+
+await queueClient.SendAsync(message);
