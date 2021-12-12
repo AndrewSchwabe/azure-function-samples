a8b5d93c9a73b19e92272ba705c3e4bd365c4c21
andrewschwabe <schwabea530@gmail.com>
2021-12-11 22:51:57 -0600

    Add notes to EventGenerator

--- src/ServiceBusEventGenerator/Program.cs
+++ src/ServiceBusEventGenerator/Program.cs
@@ -2,13 +2,15 @@
 using ServiceBusEventGenerator.Utility;
 using System.Text.Json;
 
+// Connection string found in Azure Portal to the Service Bus Resource
 var connectionString = "<ServiceBus Connection String>";
+// Name of the queue in the resource
 var queueName = "greeter";
 
-var queueClient = new QueueClient(connectionString, queueName);
-
+// Builds a mock list of people objects
 var peopleToGreet = PersonUtility.GetRandomPeople();
 
+// Creates a Service Bus message for each person in the list of peopleToGreet
 var messages = peopleToGreet
     .Select(p => new Message
     {
@@ -16,4 +18,7 @@ var messages = peopleToGreet
     })
     .ToList();
 
+// Instance of the queue client, this will be used to establish a connection to Service Bus.
+// Once the connection is established, this is the component to send the events to the queue.
+var queueClient = new QueueClient(connectionString, queueName);
 await queueClient.SendAsync(messages);
