fe83a828599253e9637a997171e71307ea317241
andrewschwabe <schwabea530@gmail.com>
2022-01-02 14:12:09 -0600

    Cosmos DB Trigger implementation

---
+++ src/Cosmos/Functions/CosmosAudit.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace Cosmos
+{
+    public static class CosmosAudit
+    {
+        [FunctionName("CosmosAudit")]
+        public static void Run([CosmosDBTrigger(
+            databaseName: "FunctionContainer",
+            collectionName: "person",
+            ConnectionStringSetting = "CosmosConnectionString",
+            CreateLeaseCollectionIfNotExists = true)]IReadOnlyList<Document> peopleDocuments,
+            ILogger log)
+        {
+            if (peopleDocuments != null && peopleDocuments.Count > 0)
+            {
+                log.LogInformation($"Documents Audited : {peopleDocuments.Count}");
+
+                foreach (var personDocument in peopleDocuments)
+                {
+                    log.LogInformation($"{personDocument.Id} : {personDocument.GetPropertyValue<string>("firstName")} : {personDocument.GetPropertyValue<string>("lastName")}");
+                }
+            }
+        }
+    }
+}
